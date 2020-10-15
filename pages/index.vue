858c96458d48b011cb14b9aae9f291a4167c22e9
Anastasia <a.shironosova@altsoft.spb.ru>
2020-10-15 18:23:50 +0300

    Initial commit

---
+++ pages/index.vue
@@ -0,0 +1,31 @@
+<template>
+    <main>
+        <header>
+            <div class="header-topbar">
+
+            </div>
+            <div class="header-logobar">
+                <div class="logo-arch">
+                    <img class="img-logo" src="~/assets/img/logo.png" />
+
+                    <div class="title-arch">
+                        <div class="title-text">САНКТ-ПЕТЕРБУРГСКИЙ ФИЛИАЛ АРХИВА <br> РОССИЙСКОЙ АКАДЕМИИ НАУК</div>
+                        <div class="title-aran">СПбФ АРАН</div>
+
+                    </div>
+                    <img class="img-book" src="~/assets/img/book.png" />
+                </div>
+
+            </div>
+            <div class="main-menu-bar">
+
+            </div>
+        </header>
+        <div class="content-wrap">
+
+        </div>
+        <footer>
+
+        </footer>
+    </main>
+</template>
