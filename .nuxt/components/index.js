4cd958d674b7f91b9c3b85687a2278bfa6937563
Anastasia Shironosova <a.shironosova@altsoft.spb.ru>
2020-10-22 16:47:41 +0300

    Меню вынесено в отдельный компонент и переделано как <nav>

---
+++ .nuxt/components/index.js
@@ -0,0 +1,9 @@
+export { default as FooterComponent } from '../..\\components\\footerComponent.vue'
+export { default as HeaderComponent } from '../..\\components\\headerComponent.vue'
+export { default as MainMenu } from '../..\\components\\mainMenu.vue'
+export { default as NewsBlock } from '../..\\components\\newsBlock.vue'
+
+export const LazyFooterComponent = import('../..\\components\\footerComponent.vue' /* webpackChunkName: "components_footerComponent" */).then(c => c.default || c)
+export const LazyHeaderComponent = import('../..\\components\\headerComponent.vue' /* webpackChunkName: "components_headerComponent" */).then(c => c.default || c)
+export const LazyMainMenu = import('../..\\components\\mainMenu.vue' /* webpackChunkName: "components_mainMenu" */).then(c => c.default || c)
+export const LazyNewsBlock = import('../..\\components\\newsBlock.vue' /* webpackChunkName: "components_newsBlock" */).then(c => c.default || c)
